5dadda7a66a4956e708c6f4ce69df6e5fe612bae
ryan <ryantxu@gmail.com>
2019-05-07 14:51:39 -0700

    use release zip

--- build/prebuild.sh
+++ build/prebuild.sh
@@ -8,12 +8,11 @@ echo "Ensure Grafana source available"
 TARGETDIR='node_modules/@grafana'
 
 if [ ! -d $TARGETDIR ]; then
-  echo "Cloning Grafana source into $TARGETDIR"
-  git clone https://github.com/grafana/grafana.git --depth 1 $TARGETDIR
+  echo "Copy grafana ui $TARGETDIR"
+  mkdir $TARGETDIR
   cd $TARGETDIR
-  yarn install --pure-lockfile
-  yarn gui:build
-  mv packages/grafana-ui/dist ui
+  wget https://gist.github.com/ryantxu/2f38b39a8a1f8441b0330a4bde34b3fd/raw/32234402d747dcabdfb6c761ea5d1028adaeb8b8/grafana-ui-6.2.0.zip
+  unzip grafana-ui-6.2.0.zip
   cd ../..
 else
   echo "Source for Grafana already present, skipping..."
