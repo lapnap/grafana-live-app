c46afd3e899e88b2c376c63fefc7cbff551ea2f6
ryan <ryantxu@gmail.com>
2019-05-08 08:39:37 -0700

    add missing files

---
+++ src/legacy/config.ts
@@ -0,0 +1,8 @@
+export class ConfigCtrl {
+  static templateUrl = 'legacy/config.html';
+
+  /** @ngInject */
+  constructor($scope: any, $injector: any) {
+    console.log('ConfigCtrl', this);
+  }
+}
