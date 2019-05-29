4ae5de4292831e27e97e08baaa7469ed68e1c396
ryan <ryantxu@gmail.com>
2019-05-28 17:17:55 -0700

    canvas panel

--- src/panels/events/EventsPanel.tsx
+++ src/panels/events/EventsPanel.tsx
@@ -1,25 +1,40 @@
 // Libraries
-import React, {PureComponent} from 'react';
+import React, {PureComponent, CSSProperties} from 'react';
 
 // Types
 import {EventsOptions} from './types';
 import {PanelProps} from '@grafana/ui';
-import {CanvasElement} from './CanvasElement';
+import {CanvasElement, CanvasMouseCallback, MouseEvtType} from './CanvasElement';
 
 export interface Props extends PanelProps<EventsOptions> {}
 
 interface State {
-  html: string;
+  selection?: CanvasMouseCallback;
 }
 
 export class EventsPanel extends PureComponent<Props, State> {
   constructor(props: Props) {
     super(props);
-
-    this.state = {
-      html: 'xxxx',
-    };
+    this.state = {};
   }
+
+  onMouseEvent = (info: CanvasMouseCallback) => {
+    let selection: CanvasMouseCallback | undefined = undefined;
+    if (info.type === MouseEvtType.drag) {
+      selection = info;
+    } else if (info.type === MouseEvtType.up) {
+      alert('TODO somehow select: ' + info);
+    }
+
+    // Force a redraw
+    if (this.state.selection !== selection) {
+      if (selection) {
+        this.setState({selection});
+      }
+    } else {
+      //   console.log( 'MOUSE:', info.type, info.percentX );
+    }
+  };
 
   counter = 0;
 
@@ -41,13 +56,55 @@ export class EventsPanel extends PureComponent<Props, State> {
     this.counter++;
   };
 
-  // viewbox: <min-x> <min-y> <width> <height>
+  renderSelection() {
+    const {selection} = this.state;
+    if (!selection) {
+      return;
+    }
+
+    const sX = selection.start!.offsetX;
+    const cX = selection.offsetX;
+
+    const min = Math.min(sX, cX);
+    const max = Math.max(sX, cX);
+    const width = Math.max(max - min, 2);
+
+    console.log('SELECTION Changed:', width, min, max);
+
+    const sss: CSSProperties = {
+      height: this.props.height,
+      border: '2px solid green',
+      left: min + 'px',
+      width: width + 'px',
+      position: 'relative',
+    };
+    return <div style={sss} />;
+  }
+
   render() {
+    const {width, height} = this.props;
+
     return (
       <div>
-        TODO! show events.
-        <br />
-        <CanvasElement {...this.props} width="100%" height={25} draw={this.draw} />
+        <div
+          style={{
+            position: 'fixed',
+            border: '1px solid red',
+            width,
+            height,
+            pointerEvents: 'none',
+          }}
+        >
+          {this.renderSelection()}
+        </div>
+
+        <CanvasElement
+          {...this.props}
+          width="100%"
+          height={25}
+          draw={this.draw}
+          onMouseEvent={this.onMouseEvent}
+        />
       </div>
     );
   }
